58464dda523167a34d55caf6f159e30ed71fc340
Agustin Gramajo <agustingramajo@gmail.com>
2021-04-17 18:44:09 -0300

    Odin exercises

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,8 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function(numf) {
+  return Math.round((numf-32)*5/9*10)/10
 }
 
-const ctof = function() {
+const ctof = function(numc) {
+  return Math.round((numc*9/5+32)*10)/10;
 
 }
 
